9493dd7b8326500cbe87999933b63056790ed050
Vinicius SoaresDev <costavinicius084@gmail.com>
2023-10-21 17:31:12 -0300

    second commit

---
+++ js/checkEndGame.js
@@ -0,0 +1,8 @@
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if (disabledCards.length == 20) {
+        clearInterval(this.loop);
+        alert('Parabéns,', $, { spanPlayer, : .innerHTML }, '! Seu tempo foi: ', $, {});
+    }
+};
